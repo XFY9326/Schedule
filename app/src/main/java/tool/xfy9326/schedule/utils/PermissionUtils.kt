c68e628ee405bcf5fbdacf040ef8d216243ffdc9
XFY9326 <1069665464@qq.com>
2021-02-19 20:01:09 +0800

    部分布局调整
    Signed-off-by: XFY9326 <1069665464@qq.com>

--- app/src/main/java/tool/xfy9326/schedule/utils/PermissionUtils.kt
+++ app/src/main/java/tool/xfy9326/schedule/utils/PermissionUtils.kt
@@ -6,14 +6,12 @@ import android.content.pm.PackageManager
 import androidx.activity.result.ActivityResultLauncher
 import androidx.core.content.ContextCompat
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.withContext
+import tool.xfy9326.schedule.kt.withTryLock
 
 
 object PermissionUtils {
-    private const val PERMISSION_REQUEST_TIMEOUT = 500L
-
     private val permissionRequestLock = Mutex()
 
     suspend fun checkCalendarPermission(context: Context, launcher: ActivityResultLauncher<Array<String>>) =
@@ -26,29 +24,22 @@ object PermissionUtils {
         return true
     }
 
-    private suspend fun checkPermission(context: Context, launcher: ActivityResultLauncher<Array<String>>, permissions: Array<String>) =
+    private suspend fun checkPermission(context: Context, launcher: ActivityResultLauncher<Array<String>>, permissions: Array<String>): Boolean =
         withContext(Dispatchers.Main.immediate) {
-            if (permissionRequestLock.tryLock()) {
-                try {
-                    val invalidPermissions = ArrayList<String>()
-                    for (permission in permissions) {
-                        if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED
-                        ) {
-                            invalidPermissions.add(permission)
-                        }
+            permissionRequestLock.withTryLock {
+                val invalidPermissions = ArrayList<String>()
+                for (permission in permissions) {
+                    if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        invalidPermissions.add(permission)
                     }
-                    return@withContext if (invalidPermissions.isEmpty()) {
-                        true
-                    } else {
-                        launcher.launch(invalidPermissions.toTypedArray())
-                        false
-                    }
-                } finally {
-                    delay(PERMISSION_REQUEST_TIMEOUT)
-                    permissionRequestLock.unlock()
                 }
-            } else {
-                return@withContext false
-            }
+                return@withTryLock if (invalidPermissions.isEmpty()) {
+                    true
+                } else {
+                    launcher.launch(invalidPermissions.toTypedArray())
+                    false
+                }
+            } ?: false
         }
 }
