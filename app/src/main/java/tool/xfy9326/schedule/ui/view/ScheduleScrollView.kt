d27f705c4208cb85516d552a48fdcbe0b1b7c4aa
XFY9326 <1069665464@qq.com>
2021-05-03 11:51:38 +0800

    使用NestedScrollView替换ScrollView
    Signed-off-by: XFY9326 <1069665464@qq.com>

--- app/src/main/java/tool/xfy9326/schedule/ui/view/ScheduleScrollView.kt
+++ app/src/main/java/tool/xfy9326/schedule/ui/view/ScheduleScrollView.kt
@@ -4,10 +4,9 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ScrollView
+import androidx.core.widget.NestedScrollView
 
-class ScheduleScrollView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0, defStyleRes: Int = 0) :
-    ScrollView(context, attrs, defStyleAttr, defStyleRes) {
+class ScheduleScrollView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : NestedScrollView(context, attrs, defStyleAttr) {
     init {
         isFillViewport = true
         overScrollMode = OVER_SCROLL_NEVER
