287afd689ece101293779fd6fa546e844911faf6
XFY9326 <1069665464@qq.com>
2021-09-14 12:03:04 +0100

    Update Version
    Signed-off-by: XFY9326 <1069665464@qq.com>

--- BuildPlugin/src/main/kotlin/tool/xfy9326/build/plugin/config/AndroidConfig.kt
+++ BuildPlugin/src/main/kotlin/tool/xfy9326/build/plugin/config/AndroidConfig.kt
@@ -5,6 +5,6 @@ object AndroidConfig {
     const val targetSdk = 31
     const val minSdk = 23
     const val applicationId = "tool.xfy9326.schedule"
-    const val versionCode = 23
-    const val versionName = "1.3.4"
+    const val versionCode = 24
+    const val versionName = "1.3.5"
 }
