387b1aec564b8b64f2c885431489235be82ed665
XFY9326 <1069665464@qq.com>
2022-04-22 14:13:40 +0100

    Update Kotlin and KSP
    Signed-off-by: XFY9326 <1069665464@qq.com>

--- AnnotationKSP/build.gradle.kts
+++ AnnotationKSP/build.gradle.kts
@@ -20,5 +20,5 @@ tasks.compileKotlin {
 dependencies {
     implementation(project(path = ":Annotation"))
     implementation("com.squareup:kotlinpoet-ksp:1.10.2")
-    implementation("com.google.devtools.ksp:symbol-processing-api:1.6.20-1.0.5")
+    implementation("com.google.devtools.ksp:symbol-processing-api:1.6.21-1.0.5")
 }
