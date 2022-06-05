9b2092b73a39cb881cbb559a4f2d6cefc2219087
xfy9326 <xfy9326@qq.com>
2022-06-05 15:44:12 +0100

    修改更新脚本

--- CodingPublish.py
+++ CodingPublish.py
@@ -3,14 +3,30 @@ import os
 import json
 import requests
 from requests.auth import HTTPBasicAuth
-from typing import Optional
+from typing import Optional, Dict
 
 release_dir = "app/release"
 release_config = f"{release_dir}/output-metadata.json"
 product_name = "tool.xfy9326.schedule.apk"
 publish_url = f"https://XFY9326-generic.pkg.coding.net/Schedule/release/{product_name}?version=%d"
-publish_user = os.getenv("CODING_SCHEDULE_RELEASE_USER_NAME")
-publish_password = os.getenv("CODING_SCHEDULE_RELEASE_USER_PASSWORD")
+
+def read_properties(file_path: str) -> Dict[str, str]:
+    result = {}
+    with open(file_path, "r", encoding="UTF-8") as f:
+        line = f.readline()
+        while line != '':
+            line = line.strip()
+            if not line.startswith("#") and "=" in line:
+                divider_index = line.index("=")
+                key = line[:divider_index].strip()
+                value = line[divider_index + 1:].strip()
+                result[key] = value
+            line = f.readline()
+    return result
+
+local_properties = read_properties("local.properties")
+publish_user = local_properties["coding.release.username"]
+publish_password = local_properties["coding.release.password"]
 
 
 def loads_config() -> Optional[dict]:
