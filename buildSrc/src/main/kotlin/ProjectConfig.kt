c94060cf5004a74f8cb0f3af281e7136d331c185
XFY9326 <xfy9326@qq.com>
2023-05-26 17:17:23 +0100

    Update v1.4.7

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -6,8 +6,8 @@ object ProjectConfig {
     const val targetSdk = 33
     const val minSdk = 23
     const val applicationId = "tool.xfy9326.schedule"
-    const val versionCode = 35
-    const val versionName = "1.4.6"
+    const val versionCode = 36
+    const val versionName = "1.4.7"
 
     val javaVersion = JavaVersion.VERSION_17
 }
